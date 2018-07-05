9f834ef86ea91ae8f6647b387bc2147b806187ae
Benjamin Bestmann <b.bestmann@gmail.com>
2018-07-06 00:02:54 +0300

    Basic implementation for ingredients

--- api/controllers/ingredients.js
+++ api/controllers/ingredients.js
@@ -2,13 +2,18 @@
 
 const express = require('express');
 const router = express.Router();
+const Ingredient = require('../models/index').ingredient;
 
 router.get('/', (req, res) => {
-	return res.json({ 'status': 'list of all ingredients is not supported yet' });
+	Ingredient.findAll()
+		.then(ingredients => res.json(ingredients))
+		.catch(error => res.status(500).json({'name': error.name, 'message': error.message}));
 });
 
 router.post('/', (req, res) => {
-	return res.json({ 'status': 'adding ingredients is not supported yet'})
+	Ingredient.create(req.body)
+		.then(data => res.status(201).json(data))
+		.catch(error => res.status(500).json({'name': error.name, 'message': error.message}));
 });
 
 module.exports = router;
