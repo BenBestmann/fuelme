240812a7e3ccc965ddfd26a07f7a7907445cd572
Benjamin Bestmann <b.bestmann@gmail.com>
2018-07-07 14:25:29 +0300

    Improve db schema for ingredients

--- api/models/ingredient.js
+++ api/models/ingredient.js
@@ -2,7 +2,16 @@
 
 module.exports = (sequelize, DataTypes) => {
 	const Ingredient = sequelize.define('ingredient', {
-		name: DataTypes.STRING
+		name: { type: DataTypes.STRING, allowNull: false },
+		seasonStartsAt: { type: DataTypes.INTEGER, defaultValue: 0 },
+		seasonEndsAt: { type: DataTypes.INTEGER, defaultValue: 11 },
+		isBasic: { type: DataTypes.BOOLEAN, defaultValue: null },
+		isVegan: { type: DataTypes.BOOLEAN, defaultValue: null },
+		isMeat: { type: DataTypes.BOOLEAN, defaultValue: null },
+		isFish: { type: DataTypes.BOOLEAN, defaultValue: null },
+		isVegetarian: { type: DataTypes.BOOLEAN, defaultValue: null },
+		hasLactose: { type: DataTypes.BOOLEAN, defaultValue: null },
+		hasGluten: { type: DataTypes.BOOLEAN, defaultValue: null }
 	});
 	return Ingredient;
 };
